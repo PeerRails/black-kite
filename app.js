3accacc15568a555a98f66f6450b73cf641771ef
Peer Rails <peer@omck.ws>
2013-08-14 19:05:39 +0400

    add some error handlers, fix konaChan func

--- app.js
+++ app.js
@@ -3,39 +3,44 @@ var twitter = require('ntwitter'),
 	path = require('path'),
 	imgur = require('imgur-node-api');
 
-//Your twitter credentials here
+//Put Your Twitter credentials here
 var twit = new twitter({
   consumer_key: 'consumer_key',
-  consumer_secret: 'consumer_secret',
+  consumer_secret: 'consumer_secretc',
   access_token_key: 'access_token_key',
   access_token_secret: 'access_token_secret'
 });
-
-imgur.setClientID('ClientID'); //Set your ClientID Here
+//Put Your imgur Client ID here
+imgur.setClientID('ClientID');
 
 var konaChan = function () {
-	site = 'http://konachan.com/post.json';
+	var site = 'http://konachan.com/post.json';
 	var img_link;
 	request(site, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			data = JSON.parse(body);			
-			img_link = data[0].file_url;
-			uploadFile(img_link);
-		} else {console.log(err)};
+			data = JSON.parse(body);
+			img = data[0].sample_url;
+			uploadFile(img);
+		} else if (error) {console.log(timeStamp()+' '+err)};
 	});
 };
 
 var uploadFile = function (file_link) {
 	imgur.upload(file_link,function(err,res){
-		console.log(res.data)
-    	updateStat(res.data.link)
+		if (err) {console.log(timeStamp()+' '+err)};
+		if (res.status !== 200) {console.log(res)};
+    	if (!err && res.status == 200) {updateStat(res.data.link)};
 	});
 }
 
 var updateStat = function (img_link) {
 	twit.updateStatus(img_link,
     function (err, data) {
-      console.log(timeStamp()+': '+'Uploaded file '+img_link);
+    	if (err) {console.log(timeStamp()+' '+err)} else {
+		console.log(timeStamp()+' '+'Uploaded file '+img_link)};
+		request(img_link, function (error, response, body) {
+			//:3 Nothing here
+		});
     }
   );
 }
@@ -47,5 +52,7 @@ var timeStamp = function () {
 }
 
 
-setInterval(function() {konaChan();}, 600000);
-console.log(timeStamp()+' : '+'Starting upload in 10 minutes');
+setInterval(function() {
+	konaChan();
+}, 600000);
+console.log(timeStamp()+' '+'Starting upload in 10 minutes');
