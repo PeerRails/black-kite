d02dd9c353b9fa324bd78f6489c256592f28c5e5
Peer Rails <peer@omck.ws>
2013-08-16 18:21:05 +0400

    add Danbooru Parser,split updateStat & uploadfile

--- app.js
+++ app.js
@@ -13,34 +13,55 @@ var twit = new twitter({
 
 imgur.setClientID(config.cid);
 
+var Danbooru = function (site_link) {
+	this.site_link = site_link;
+	this.api_link = '/posts.json';
+};
+
+var donmaius = new Danbooru('http://donmai.us');
+
+var parseDanbooru = function (booru) {
+	var site = booru.site_link+booru.api_link;
+	console.log(timeStamp()+' Uploading from '+booru.site_link);
+	var img_link;
+	request(site, function (error, response, body){
+		if (!error && response.statusCode == 200) {
+			data = JSON.parse(body);
+			img = data[0].file_url;
+			uploadFile(booru.site_link+img,booru.site_link);
+		} else if (error) {console.log(timeStamp()+' '+error)};
+		if (response.statusCode !== 200) {console.log(timeStamp()+' '+response.statusCode)}
+	});
+};
+
 var konaChan = function () {
 	var site = 'http://konachan.com/post.json';
+	console.log(timeStamp()+' Upload from KonaChan.com');
 	var img_link;
 	request(site, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			data = JSON.parse(body);
 			img = data[0].sample_url;
-			uploadFile(img);
+			uploadFile(img, 'http://konachan.com');
 		} else if (error) {console.log(timeStamp()+' '+err)};
+		if (response.statusCode !== 200) {console.log(timeStamp()+' '+response)};
 	});
 };
 
-var uploadFile = function (file_link) {
+var uploadFile = function (file_link, site_link) {
 	imgur.upload(file_link,function(err,res){
 		if (err) {console.log(timeStamp()+' '+err)};
 		if (res.status !== 200) {console.log(res)};
-    	if (!err && res.status == 200) {updateStat(res.data.link)};
+    	if (!err && res.status == 200) {
+    		tweet_post = res.data.link + ' from '+ site_link;
+    		twit.updateStatus(tweet_post, function (error, data){
+    			if (error) {console.log(timeStamp()+' '+err)} else {
+    				console.log(timeStamp()+' '+'Upload file '+res.data.link+' from '+ site_link)
+    			}
+    		});
+    	};
 	});
-}
-
-var updateStat = function (img_link) {
-	twit.updateStatus(img_link,
-    function (err, data) {
-    	if (err) {console.log(timeStamp()+' '+err)} else {
-		console.log(timeStamp()+' '+'Uploaded file '+img_link)};
-    }
-  );
-}
+};
 
 var timeStamp = function () {
 	date = new Date();
@@ -48,9 +69,11 @@ var timeStamp = function () {
 	return time;
 };
 
-konaChan();
+console.log(timeStamp()+' '+'Start upload in 33 minutes');
+
+//konaChan();
+parseDanbooru(donmaius);
+
 setInterval(function() {
 	konaChan();
 }, config.timelimit); 
-
-console.log(timeStamp()+' '+'Starting upload in 33 minutes');
